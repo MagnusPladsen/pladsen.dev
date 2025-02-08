d8d80a72c8d4288c1ed38af729968a52ac5b7435
Magnus Pladsen <magnus.pladsen@abaris.no>
2025-02-08 13:29:02 +0100

    Updated with enviroment variable for next backend server

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -3,34 +3,35 @@ import { ContactInfo } from '@/lib/contactInfo';
 import Socials from './Socials.component';
 
 const getContactInfo = async (): Promise<ContactInfo> => {
-  const res = await fetch('http://localhost:3000/api/contactInfo');
-  const data = await res.json();
-  return data;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/contactInfo`, {
+    });
+    const data = await res.json();
+    return data;
 };
 
 export default async function Header() {
-  const contactInfo = await getContactInfo();
+    const contactInfo = await getContactInfo();
 
-  return (
-    <div className="flex flex-col">
-      <div className="flex justify-between items-start w-full gap-8">
-        <div>
-          <h1 className="text-6xl font-bold mb-2">
-            {contactInfo.firstName} <span className="text-primary">{contactInfo.lastName}</span>
-          </h1>
-          <h2 className="text-2xl mb-4">{contactInfo.role}</h2>
+    return (
+        <div className="flex flex-col">
+            <div className="flex justify-between items-start w-full gap-8">
+                <div>
+                    <h1 className="text-6xl font-bold mb-2">
+                        {contactInfo.firstName} <span className="text-primary">{contactInfo.lastName}</span>
+                    </h1>
+                    <h2 className="text-2xl mb-4">{contactInfo.role}</h2>
+                </div>
+                <div className="hidden md:block -mb-8">
+                    <Image
+                        src="/avatar.png"
+                        alt={contactInfo.fullName}
+                        width={200}
+                        height={200}
+                        className="rounded-full aspect-square w-[140px] h-[140px] object-cover "
+                    />
+                </div>
+            </div>
+            <Socials socials={contactInfo.socials} />
         </div>
-        <div className="hidden md:block -mb-8">
-          <Image
-            src="/avatar.png"
-            alt={contactInfo.fullName}
-            width={200}
-            height={200}
-            className="rounded-full aspect-square w-[140px] h-[140px] object-cover "
-          />
-        </div>
-      </div>
-      <Socials socials={contactInfo.socials} />
-    </div>
-  );
+    );
 }
